f1c54baf88b07b6f2f96ada20ae86c459b0f0f83
bhavik-crest <154506549+bhavik-crest@users.noreply.github.com>
2025-11-06 15:36:25 +0530

    Reset Database config

--- backend/app/database.py
+++ backend/app/database.py
@@ -1,22 +1,18 @@
 import os
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_async_engine(
-    DATABASE_URL,
-    echo=True,
-    future=True,
-    connect_args={"sslmode": "require"}
-)
+engine = create_async_engine(DATABASE_URL, echo=True)
 
-async_session = sessionmaker(
-    engine,
-    expire_on_commit=False,
-    class_=AsyncSession
+AsyncSessionLocal = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
 )
 
 async def get_db():
-    async with async_session() as session:
+    async with AsyncSessionLocal() as session:
         yield session
